4d4aef271e43ab2d4c43b864bbafeb1064f83204
Jeffrey <jeffrey.quan@hotmail.com>
2022-11-05 15:14:29 +1100

    feat: make api call from frontend to backend

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
+
+
+@app.route("/api")
 def hello_world():
-  return "Hello, World!"
+  return jsonify({
+    'message': "Hello, cross-origin-world!"
+  })
