8ada81173a2f9193b9b43c58eabf79c25267ac9f
jeffreyquan <jeffrey_quan@mckinsey.com>
2023-04-17 12:19:57 +1000

    fix: issue with converting string to sql statements

--- backend/app/db/queries.py
+++ backend/app/db/queries.py
@@ -1,17 +1,10 @@
 from  ..main import database_connection
+from sqlalchemy.sql import text
 
 def list_tasks():
-  return database_connection().execute_query(
-    f"""
-      SELECT * FROM todo.tasks
-    """
-  )
+  return database_connection().execute_query(text("SELECT * FROM todo.tasks"))
 
 def insert_task(description):
-  return database_connection().execute_write_operation(
-    f"""
-      INSERT INTO todo.tasks (description)
-      VALUES (:description)
-    """,
-    {"description": description}
-  )
+  values =  {"description": description}
+  statement = text("INSERT INTO todo.tasks (description) VALUES (:description)")
+  return database_connection().execute_write_operation(statement, values)
